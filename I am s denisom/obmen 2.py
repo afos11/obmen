2e4bffef45a3cf31161e6f788d8d612ef20e08bc
Софья <askovasofa2@gmail.com>
2024-11-18 23:25:17 +0600

    создана функция exchange для получения из интернета курсов валют

--- I am s denisom/obmen 2.py
+++ I am s denisom/obmen 2.py
@@ -4,6 +4,26 @@ import requests
 import json
 
 
+def exchange():
+    code=entry.get().upper()
+
+    if code:
+        try:
+            response=requests.get('https://open.er-api.com/v6/latest/USD')#адрес сайта
+            response.raise_for_status()
+            data=response.json()#словарь в  data
+
+            if code in data ['rates']: #сущ та валюта которую ввел пользователь
+                exchange_rate=data['rates'][code] #курс обмена exchange_rate выбираем словарь data['rates'] значение по ключу [code]
+                mb.showinfo('Курс обмена',
+                            f'Курс к доллару: {exchange_rate} {code} за 1 доллар.')
+            else:
+                mb.showerror('Ошибка', f'Такая валюта  {code}не найдена')
+
+        except Exception as e:
+            mb.showerror('Ошибка', f'Произошла ошибка {e}')
+    else:
+        mb.showwarning('Внимание!','Вы не ввели код валюты!')# предупреждение
 
 
 
