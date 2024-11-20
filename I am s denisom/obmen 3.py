1e29cdaf4009d2781db164b0ff0c47c9034b0f05
Софья <askovasofa2@gmail.com>
2024-11-20 12:31:19 +0600

    в метку добавлены названия валют

--- I am s denisom/obmen 3.py
+++ I am s denisom/obmen 3.py
@@ -1,11 +1,19 @@
 from tkinter import *
 from tkinter import messagebox as mb
 from tkinter import ttk
+from tkinter.ttk import Combobox
 
 import requests
 import json
 
-from katya.main3_vibor import combobox
+
+
+def update_c_label(event):
+    code=combobox.get()
+    name=cur[code]
+    c_label.config(text=name)
+
+
 
 
 def exchange():
@@ -19,8 +27,9 @@ def exchange():
 
             if code in data ['rates']: #сущ та валюта которую ввел пользователь
                 exchange_rate=data['rates'][code] #курс обмена exchange_rate выбираем словарь data['rates'] значение по ключу [code]
+                c_name=cur[code]
                 mb.showinfo('Курс обмена',
-                            f'Курс к доллару: {exchange_rate:.2f} {code} за 1 доллар.')
+                            f'Курс к доллару: {exchange_rate:.2f} {c_name} за 1 доллар.')
             else:
                 mb.showerror('Ошибка', f'Такая валюта  {code}не найдена')
 
@@ -29,7 +38,17 @@ def exchange():
     else:
         mb.showwarning('Внимание!','Вы не ввели код валюты!')# предупреждение
 
-
+cur = {
+    "EUR": "Евро","JPY": "Японская йена",
+    "GBP": "Британский фунт стерлингов",
+    "AUD": "Австралийский доллар",
+    "CAD": "Канадский доллар",
+    "CHF": "Швейцарский франк",
+    "CNY": "Китайский юань",
+    "RUB": "Российский рубль",
+    "KZT": "Казахстанский тенге",
+    "UZS": "Узбекский сум"
+}
 
 window=Tk()
 window.title('Курсы обмена валют! ')
@@ -38,15 +57,16 @@ window.geometry('500x500')
 
 Label(text='Выберете код валюты: ').pack(padx=10,pady=10)
 
-cur=["EUR", "JPY", "GBP", "AUD", "CAD", "CHF", "CNY", "RUB", "KZT","UZS"]
+#cur=["EUR", "JPY", "GBP", "AUD", "CAD", "CHF", "CNY", "RUB", "KZT","UZS"]
 
-combobox=ttk.Combobox(values=cur)
+combobox=ttk.Combobox(values=list(cur.keys()))
 combobox.pack(padx=10,pady=10)
+combobox.bind('<<ComboboxSelected>>',update_c_label)
 
 
-
-#entry=Entry()
-#entry.pack(padx=10,pady=10)
+#создаем метку чтобы вывести название валюты
+c_label=ttk.Label()
+c_label.pack(padx=10,pady=10)
 
 
 Button(text='Получить курс обмена к доллару!', command=exchange).pack()
